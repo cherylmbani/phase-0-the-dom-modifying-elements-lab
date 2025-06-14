5705db95f53b1573c3c549bf68d5d407d98402e6
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-06-14 20:12:51 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+// creating h1 element
+const newHeader = document.createElement("h1");
+
+//setting an attribute
+newHeader.id = "victory";
+
+// adding content inside an element
+newHeader.textContent = "Cheryl is the champion";
+
+// appending a child element
+document.body.appendChild(newHeader);
